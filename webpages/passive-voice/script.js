1335a9d9a82c7bf527f0047c41e133c358b0c2b4
Randi Kurniawan <118991426+hoshikatsumi@users.noreply.github.com>
2023-11-15 11:27:46 +0800

    Added script checking

--- webpages/passive-voice/script.js
+++ webpages/passive-voice/script.js
@@ -1,3 +1,23 @@
+function scriptChecking() {
+  var wrapperDiv = document.getElementById('scriptCheck');
+  var existingImage = document.getElementById('noScript');
+  var newImage = document.createElement('img');
+  newImage.src = 'assets/images/complete.svg';
+  var currentText = document.querySelector('.exercise-caution-wrapper-margin-script-text');
+  var newParagraph = document.createElement('p');
+  newParagraph.textContent = 'Script is currently enabled.';
+  wrapperDiv.replaceChild(newImage, existingImage);
+  currentText.replaceChild(newParagraph, currentText.firstElementChild);
+  var exerciseBottomBreak = document.getElementsByClassName("exercise-caution-wrapper-margin-hr-bottom");
+  var exerciseAttemptButton = document.getElementsByClassName("exercise-caution-wrapper-margin-button");
+  exerciseBottomBreak[0].style.display = "block";
+  exerciseAttemptButton[0].style.display = "flex";
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  scriptChecking();
+});
+
 function showExercise() {
   var exerciseMainElements = document.getElementsByClassName("exercise-main");
   var exerciseCautionElements = document.getElementsByClassName("exercise-caution");
@@ -7,24 +27,24 @@ function showExercise() {
 
 function getHiddenProp() {
   var prefixes = ['webkit', 'moz', 'ms', 'o'];
-  // If 'hidden' is natively supported, just return it
   if ('hidden' in document) return 'hidden';
-  // Otherwise loop over all known prefixes until it finds one
   for (var c = 0; c < prefixes.length; c++) {
     if ((prefixes[i] + 'Hidden') in document) {
       return prefixes[i] + 'Hidden';
     }
   }
-  // Otherwise it is not supported
   return null;
 }
 
 function handleVisibilityChange() {
   if (document.visibilityState === 'visible') {
-    // Page is visible, reload the page
     window.location.reload(true);
+  } else if (document.visibilityState !== 'visible') {
+    var exerciseMainElements = document.getElementsByClassName("exercise-main");
+    var exerciseCautionElements = document.getElementsByClassName("exercise-caution");
+    exerciseMainElements[0].style.display = "none";
+    exerciseCautionElements[0].style.display = "none";
   }
 }
 
-// Listen for visibility change events
 document.addEventListener('visibilitychange', handleVisibilityChange);
