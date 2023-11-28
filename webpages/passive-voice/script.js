e059800988a601f6ca1820d0e2e7c2f77fe4d20b
Randi Kurniawan <118991426+hoshikatsumi@users.noreply.github.com>
2023-11-28 07:48:45 +0700

    Reverted changes

--- webpages/passive-voice/script.js
+++ webpages/passive-voice/script.js
@@ -1,38 +1,50 @@
-const minuteVar = 15;
-const hourVar = 13;
-const dateVar = 27;
-const monthVar = 10;
-const yearVar = 2023;
-
-const expMin = 29;
-const expHour = 14;
-const expDate = 27;
-const expMonth = 10;
-const expYear = 2023;
-
-function timeChecking(currentDate) {
-  var currentDate = new Date();
-  var setDate = new Date(yearVar, monthVar, dateVar, hourVar, minuteVar);
-  var expireDate = new Date(expYear, expMonth, expDate, expHour, expMin);
-  var exerciseBefore = document.getElementsByClassName("container-content-exercise-not-begin");
-  var exerciseToDo = document.getElementsByClassName("container-content-exercise");
-  var exerciseFinished = document.getElementsByClassName("container-content-exercise-has-finished");
-  if (currentDate < setDate) {
-    exerciseBefore[0].style.display = "block";
-    exerciseToDo[0].style.display = "none";
-    exerciseFinished[0].style.display = "none";
-  } else if (currentDate > setDate && currentDate < expireDate) {
-    exerciseBefore[0].style.display = "none";
-    exerciseToDo[0].style.display = "block";
-    exerciseFinished[0].style.display = "none";
-  } else if (currentDate > expireDate) {
-    exerciseBefore[0].style.display = "none";
-    exerciseToDo[0].style.display = "none";
-    exerciseFinished[0].style.display = "block";
-  }
-  setTimeout(() => timeChecking(), 1000);
+function scriptChecking() {
+  var wrapperDiv = document.getElementById('scriptCheck');
+  var existingImage = document.getElementById('noScript');
+  var newImage = document.createElement('img');
+  newImage.src = 'assets/images/complete.svg';
+  var currentText = document.querySelector('.exercise-caution-wrapper-margin-script-text');
+  var newParagraph = document.createElement('p');
+  newParagraph.textContent = 'Script is currently enabled.';
+  wrapperDiv.replaceChild(newImage, existingImage);
+  currentText.replaceChild(newParagraph, currentText.firstElementChild);
+  var exerciseBottomBreak = document.getElementsByClassName("exercise-caution-wrapper-margin-hr-bottom");
+  var exerciseAttemptButton = document.getElementsByClassName("exercise-caution-wrapper-margin-button");
+  exerciseBottomBreak[0].style.display = "block";
+  exerciseAttemptButton[0].style.display = "flex";
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  timeChecking(currentDate);
+  scriptChecking();
 });
+
+function showExercise() {
+  var exerciseMainElements = document.getElementsByClassName("exercise-main");
+  var exerciseCautionElements = document.getElementsByClassName("exercise-caution");
+  exerciseMainElements[0].style.display = "flex";
+  exerciseCautionElements[0].style.display = "none";
+}
+
+function getHiddenProp() {
+  var prefixes = ['webkit', 'moz', 'ms', 'o'];
+  if ('hidden' in document) return 'hidden';
+  for (var c = 0; c < prefixes.length; c++) {
+    if ((prefixes[i] + 'Hidden') in document) {
+      return prefixes[i] + 'Hidden';
+    }
+  }
+  return null;
+}
+
+function handleVisibilityChange() {
+  if (document.visibilityState === 'visible') {
+    window.location.reload(true);
+  } else if (document.visibilityState !== 'visible') {
+    var exerciseMainElements = document.getElementsByClassName("exercise-main");
+    var exerciseCautionElements = document.getElementsByClassName("exercise-caution");
+    exerciseMainElements[0].style.display = "none";
+    exerciseCautionElements[0].style.display = "none";
+  }
+}
+
+document.addEventListener('visibilitychange', handleVisibilityChange);
